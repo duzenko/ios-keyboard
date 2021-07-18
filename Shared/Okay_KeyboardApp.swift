7993a1d70254376944c6c68e9a2b0765c6abc6da
Anton Duzenko <anton.duzenko@gmail.com>
2021-07-18 04:41:11 -0600

    Initial Commit

---
+++ Shared/Okay_KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Okay_KeyboardApp.swift
+//  Shared
+//
+//  Created by a on 18.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct Okay_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
